8338f3b601c020ec276308690cfa4fd2b377a57b
Tyler Whitaker <tywhit@outlook.com>
2020-08-06 09:47:06 -0600

    Created functions to separate time into arrays

--- time_calculator.py
+++ time_calculator.py
@@ -1,7 +1,17 @@
 def add_time(start, duration):
+    start = [separate_hour(start),separate_minute(start)]
+    duration = [[separate_hour(duration),separate_minute(duration)]]
+
+    new_time = 0
+    return new_time    
+
+def separate_hour(time):
+    return time.split(sep=":")[0]
+
+def separate_minute(time):
+    return time.split(sep=":")[1]
 
 
 
 
 
-    return new_time
