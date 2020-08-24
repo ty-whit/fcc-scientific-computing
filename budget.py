3bd3035aa8d6ad89a64db5bf2144b8668922e607
Tyler Whitaker <tywhit@outlook.com>
2020-08-24 13:07:49 -0600

    Added function skeleton

--- budget.py
+++ budget.py
@@ -1,7 +1,22 @@
 class Category:
+    def __init__(self):
+        pass
 
+    def deposit(self):
+        pass
 
+    def withdraw(self): 
+        pass 
 
+    def get_balance(self):
+        pass
+
+    def transfer(self): 
+        pass
+
+    def check_funds(self): 
+        pass
 
 
 def create_spend_chart(categories):
+    pass
