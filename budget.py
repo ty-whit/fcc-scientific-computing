9b673cc0e4f0c288ba171f5737cb5344335936d3
Tyler Whitaker <tywhit@outlook.com>
2020-08-24 13:20:07 -0600

    Added code for withdraw

--- budget.py
+++ budget.py
@@ -1,22 +1,25 @@
 class Category:
     def __init__(self,name):
         self.name = name
-        self.ledger = dict()
+        self.ledger = [ ]
         return
 
-    def deposit(self):
-        pass
+    def deposit(self, amount, description):
+        self.ledger.append({"amount": amount, "description": description})
+        return
 
-    def withdraw(self): 
-        pass 
-
+    def withdraw(self,amount, description): 
+        if self.check_funds(amount):
+            self.ledger.append({"amount": -1 * amount, "description": description})
+        return 
+        
     def get_balance(self):
         pass
 
     def transfer(self): 
         pass
 
-    def check_funds(self): 
+    def check_funds(self,amount): 
         pass
 
 
