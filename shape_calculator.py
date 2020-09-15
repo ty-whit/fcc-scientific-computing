74ac5fc9de6763e9d42a080f9fc8904d2ad006e8
Tyler Whitaker <tywhit@outlook.com>
2020-09-15 08:56:43 -0600

    set up structure for classes

--- shape_calculator.py
+++ shape_calculator.py
@@ -1,6 +1,29 @@
 class Rectangle:
+    def __init__(self, width, height):
+        pass
+    
+    def set_width(self, width):
+        pass
 
+    def set_height(self, height):
+        pass
 
+    def get_area(self):
+        pass
 
+    def get_perimeter(self):
+        pass
 
-class Square:
+    def get_diagonal(self):
+        pass
+
+    def get_picture(self):
+        pass
+
+    def get_amount_inside(self, rect):
+        pass
+
+class Square(Rectangle):
+    def set_side(self, side):
+        pass
+
