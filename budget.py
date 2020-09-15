43b0578eb5f8f891442799b94a81ad4ab1deaed5
Tyler Whitaker <tywhit@outlook.com>
2020-09-14 20:56:46 -0600

    Added comments and minor edits to fix tests

--- budget.py
+++ budget.py
@@ -1,20 +1,31 @@
 class Category:
     def __init__(self,name):
+        # Initialize the name of the budget
         self.name = name
+        # Initialize an empty list to fill with transactions
         self.ledger = [ ]
         return
     
     def __str__(self):
+        # Print the top bar of *** with the name centered
         printString = self.name.center(30,'*') + '\n'
         
+        # for each transaction in the ledger
         for i in range(len(self.ledger)):
+            # print the description
             printString += self.ledger[i]['description'][:23].ljust(23,' ')
-            printString += str(self.ledger[i]['amount']).rjust(7,' ')
+            # and the amount of money for that item.
+            printString += ('{:.2f}'.format(float(self.ledger[i]['amount']))).rjust(7,' ')
+            # Dont forget the new line. 
             printString += '\n'
 
+        # Print total amount of money for this category.
+        printString += 'Total: ' + str('{:.2f}'.format(self.get_balance()))
         return printString
 
     def deposit(self, amount, description = ''):
+        # Add an item to the ledger. This item gets added in as a Dictionary of 
+        # amount and description. 
         self.ledger.append({'amount': amount, 'description': description})
         return
 
@@ -25,23 +36,42 @@ class Category:
         return sufficientFunds
 
     def get_balance(self):
+        # Set an initial counting variable to zero. 
         balance = 0
+        # For each transaction,
         for transaction in self.ledger:
+            # Add up the money. Deposits are positive, and withdraws are negative.
             balance += transaction['amount']
+        # Return the final amount for this category. 
         return balance
 
     def transfer(self,amount, account): 
+        # First, check to see if there are sufficient funds in this account to
+        # that could be transferred to another account. 
         sufficientFunds = self.check_funds(amount)
+
+        # If there is enough money,
         if sufficientFunds: 
+            # Withdraw the amount from here,
             self.withdraw(amount,'Transfer to ' + account.name)
+            # and make a deposit to the destination account. 
             account.deposit(amount,'Transfer from ' + self.name)
+        
+        # Return whether this account had enough funds. If true, the transfer
+        # took place. Otherwise, it did not. 
         return sufficientFunds
 
     def check_funds(self,amount): 
+        # Get the balance for this account. 
         balance = self.get_balance()
+        # Find the difference betweenthe desired withdraw, and the actual balance. 
         diff = balance - amount
+        # If that difference is negative, there is not enough money. 
         if diff < 0:
+            # Return false, as in there are not sufficient funds. 
             return False
+        # If this point is reached, the diff was positive, and therefore, there
+        # are sufficient funds. 
         return True
 
 def create_spend_chart(categories):
@@ -52,7 +82,7 @@ def create_spend_chart(categories):
     for cat in categories: 
         # Amount spent by current category  in loop
         catSpending = 0
-        print('Spending on',cat.name)
+
         # Loop through each category
         for transaction in cat.ledger:
             # If transaction was positive, it was a deposit. 
@@ -63,7 +93,6 @@ def create_spend_chart(categories):
 
             # Append total spent on current category to list created above.
         spendingList.append(catSpending)
-        print('---',catSpending)
     
     # Calculate the total spending to find percentage of each category.
     totalSpending = sum(spendingList)
@@ -124,9 +153,17 @@ def create_spend_chart(categories):
 
         printStringList.append(newLine)
 
-    finalString = printStringList[0]
+    # Logic of above loop includes a extra line of whitespace.
+    # This whitespace causes tests to fail. 
+    printStringList.pop()
 
+    # Save the first string to the final string to be returned. 
+    finalString = printStringList[0]
+    
+    # For every line in the list, starting with the second,
     for line in printStringList[1:]:
+        # Add it as a new line to the string to be returned. 
         finalString += '\n' + line
 
+    # Return the final string to be printed. 
     return finalString
