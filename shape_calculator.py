aad2723c3b6a160f6c6324a88bc80b346f9fdefd
Tyler Whitaker <tywhit@outlook.com>
2020-09-15 20:29:44 -0600

    Minor fixes and fixes to pass test

--- shape_calculator.py
+++ shape_calculator.py
@@ -5,7 +5,7 @@ class Rectangle:
         return
     
     def __str__(self):
-        return 'Rectangle(Width=' + str(self.width) + ', height=' + str(self.height) + ')'
+        return 'Rectangle(width=' + str(self.width) + ', height=' + str(self.height) + ')'
     
     def set_width(self, width):
         self.width = width
@@ -25,18 +25,24 @@ class Rectangle:
         return (self.width**2 + self.height**2)**(1/2)
 
     def get_picture(self):
+        if self.height > 50 or self.width > 50: 
+            return 'Too big for picture.'
         printString  = '*'*self.width + '\n'
         printString *= self.height
         return printString
 
     def get_amount_inside(self, rect):
-        return self.get_area() / rect.get_area()
+        return self.get_area() // rect.get_area()
 
 class Square(Rectangle):
-    def __init__(self,side):
+    def __init__(self, side):
         self.set_side(side)
         return 
-        
+
+    def __str__(self):
+        return 'Square(side=' + str(self.width) + ')'
+
+
     def set_side(self, side):
         self.set_width(side)
         self.set_height(side)
